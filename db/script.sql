bc7f7c2711cb69ec65266205f517c510d98c383f
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2024-04-29 08:22:32 -0300

    finalização dp script.db e criação dos métodos GET

--- db/script.sql
+++ db/script.sql
@@ -1,16 +1,29 @@
 --Criação do banco
 
-CREATE DATABASE aulaback;
+CREATE DATABASE harybb;
 
---Crição da tabela
+--Crição da tabela de bruxos
 
-CREATE TABLE usuarios(
+CREATE TABLE bruxos(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    idade VARCHAR(100) NOT NULL,
+    casa VARCHAR(100) NOT NULL,
+    habilidade VARCHAR(100) NOT NULL,
+    sangue VARCHAR(10) NOT NULL,
+    patrono VARCHAR(100)
 )
 
---Inserção de dados teste
+--Crição da tabela de varinhas
 
-INSERT INTO usuarios(nome, email) VALUES ('João', 'joaosantos@gmail.com');
-INSERT INTO usuarios(nome, email) VALUES ('M.Rita', 'mariarita@gmail.com');
+CREATE TABLE varinhas(
+    id SERIAL PRIMARY KEY,
+    material VARCHAR(100) NOT NULL,
+    comprimento VARCHAR(100) NOT NULL,
+    nucleo VARCHAR(100) NOT NULL,
+    fabricacao DATE NOT NULL
+)
+
+
+
+
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { Pool } = require('pg')
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'aulaback',
+    database: 'harybb',
     password: 'ds564',
     port: 5432,
 });
@@ -16,72 +16,72 @@ const pool = new Pool({
 app.use(express.json());
 
 
-app.get('/usuarios', async (req, res) => {
+app.get('/bruxos', async (req, res) => {
     try {
-        const resultado = await pool.query('SELECT * FROM usuarios')
+        const resultado = await pool.query('SELECT * FROM bruxos')
         res.json({
             total: resultado.rowCount,
-            usuarios: resultado.rows,
+            bruxos: resultado.rows,
         })
     } catch (error) {
-        console.error('Erro ao obter todos os usuários', error);
-        res.status(500).send('Erro ao obter todos os usuários')
+        console.error('Erro ao obter todos os bruxos', error);
+        res.status(500).send('Erro ao obter todos os bruxos')
     }
 })
 
 
-app.post('/usuarios', async (req, res) => {
-    try {
-        const { nome, email } = req.body;
-        await pool.query('INSERT INTO usuarios (nome, email) VALUES ($1, $2)', [nome, email])
-        res.status(201).send({ mensagem: 'Usuario criado com sucesso' })
-    } catch (error) {
-        console.error('Erro ao inserir o usuário', error);
-        res.status(500).send('Erro ao inserir o usuário')
-    }
-})
+// app.post('/usuarios', async (req, res) => {
+//     try {
+//         const { nome, email } = req.body;
+//         await pool.query('INSERT INTO usuarios (nome, email) VALUES ($1, $2)', [nome, email])
+//         res.status(201).send({ mensagem: 'Usuario criado com sucesso' })
+//     } catch (error) {
+//         console.error('Erro ao inserir o usuário', error);
+//         res.status(500).send('Erro ao inserir o usuário')
+//     }
+// })
 
 
 
-app.delete('/usuarios/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      await pool.query('DELETE FROM usuarios WHERE id = $1', [id]);
-      res.status(200).send({ mensagem: 'Usuário excluído com sucesso'});
-    } catch (error) {
-      console.error('Erro ao excluir usuário:', error);
-      res.status(500).send('Erro ao excluir usuário');
-    }
-  });
+// app.delete('/usuarios/:id', async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       await pool.query('DELETE FROM usuarios WHERE id = $1', [id]);
+//       res.status(200).send({ mensagem: 'Usuário excluído com sucesso'});
+//     } catch (error) {
+//       console.error('Erro ao excluir usuário:', error);
+//       res.status(500).send('Erro ao excluir usuário');
+//     }
+//   });
 
 
-app.put('/usuarios/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { nome, email } = req.body;
-      await pool.query('UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3', [nome, email, id]);
-      res.status(200).send({ mensagem: 'Usuário atualizado com sucesso'});
-    } catch (error) {
-      console.error('Erro ao atualizar usuário:', error);
-      res.status(500).send('Erro ao atualizar usuário');
-    }
-  });
+// app.put('/usuarios/:id', async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       const { nome, email } = req.body;
+//       await pool.query('UPDATE usuarios SET nome = $1, email = $2 WHERE id = $3', [nome, email, id]);
+//       res.status(200).send({ mensagem: 'Usuário atualizado com sucesso'});
+//     } catch (error) {
+//       console.error('Erro ao atualizar usuário:', error);
+//       res.status(500).send('Erro ao atualizar usuário');
+//     }
+//   });
 
 
-  app.get('/usuarios/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const result = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
-      if (result.rowCount === 0) {
-        res.status(404).send({ mensagem: 'Usuário não encontrado' });
-      } else {
-        res.json(result.rows[0]);
-      }
-    } catch (error) {
-      console.error('Erro ao obter usuário por ID:', error);
-      res.status(500).send('Erro ao obter usuário por ID');
-    }
-  });
+//   app.get('/usuarios/:id', async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       const result = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
+//       if (result.rowCount === 0) {
+//         res.status(404).send({ mensagem: 'Usuário não encontrado' });
+//       } else {
+//         res.json(result.rows[0]);
+//       }
+//     } catch (error) {
+//       console.error('Erro ao obter usuário por ID:', error);
+//       res.status(500).send('Erro ao obter usuário por ID');
+//     }
+//   });
   
 
 app.listen(PORT, () => {
